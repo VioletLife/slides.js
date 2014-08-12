401ae2b87d3a3445e0326596b07a4e2bff933a3e
Viktor Qvarfordt <viktor.qvarfordt@gmail.com>
2014-08-12 22:48:12 +0200

    Event listeners. Other improvements.

--- presenter.js
+++ presenter.js
@@ -2,49 +2,85 @@
 /* global $:true */
 
 var api = {
-  slides: null, // populated by init()
-  currentSlideNumber: 1,
+  config: {
+    scaling: 'superfit'
+  },
+  slides: null,
+  currentSlideNumber: null,
+  events: {
+    slideChange: []
+  },
+  on: function(eventName, eventCallback) {
+    if (this.events.hasOwnProperty(eventName)) {
+      this.events[eventName].push(eventCallback);
+    }
+  },
+  trigger: function(eventName, eventArgs) {
+    if (this.events.hasOwnProperty(eventName)) {
+      this.events[eventName].forEach(function(callback) {
+        callback(eventArgs);
+      });
+    }
+  },
   prev: function() {
+    // Reveal previous fragment or go to next slide.
     var lastVisibleFragment = this.currentSlide.find('.fragment.visible').last().removeClass('visible');
     if (!lastVisibleFragment.length) this.goToSlide(this.currentSlideNumber - 1);
   },
   next: function() {
+    // Reveal next fragment or go to next slide.
     var firstHiddenFragment = this.currentSlide.find('.fragment:not(.visible)').first().addClass('visible');
     if (!firstHiddenFragment.length) this.goToSlide(this.currentSlideNumber + 1);
   },
   goToSlide: function(n) {
-    if (this.currentSlideNumber === this.slides.length && n > this.slides.length) {
-      return;
-    }
-    if ((n > this.slides.length || n < 1)) {
-      n = 1;
-    }
-    this.currentSlideNumber = n;
-    window.location.hash = n;
+    // Do not go past last slide.
+    if (this.currentSlideNumber === this.slides.length && n > this.slides.length) return;
+    // Go to first slide if n is out of range.
+    if ((n > this.slides.length || n < 1)) n = 1;
+    if (n === this.currentSlideNumber) return;
+    this.currentSlideNumber = window.location.hash = n;
     if (this.currentSlide) this.currentSlide.removeClass('active');
-    this.currentSlide = this.getSlide(n);
-    this.currentSlide.addClass('active');
+    this.currentSlide = this.getSlide(n).addClass('active');
     this.normalize();
+    this.trigger('slideChange');
   },
   getSlide: function(n) {
     return this.slides.filter('[data-slide-number="' + n + '"]');
   },
   normalize: function() {
-    var scaleFactor = Math.min($(window).height() / this.currentSlide.outerHeight(), $(window).width() / this.currentSlide.outerWidth());
+    // Recalculate current slides scaling and position to fit viewport dimensions.
+
+    // var aspectRatio = $(window).width() / $(window).height();
+    // this.currentSlide.css('width', this.currentSlide.height() * aspectRatio);
+    var heightRatio = $(window).height() / this.currentSlide.outerHeight();
+    var widthRatio = $(window).width() / this.currentSlide.outerWidth();
+    var scaleFactor = Math.min(heightRatio, widthRatio);
+
+    // Scaling leads to flickering and blur on opacity transition in webkit.
+    // This can be compensated for by setting backface-visibility: hidden;
+    // Although, this gives constant blur.
+    if (this.config.scaling === 'superfit') {
+      this.currentSlide.css('transform', 'scale(' + scaleFactor + ')');
+    } else if (this.config.scaling === 'fit') {
+      this.currentSlide.css('transform', 'scale(' + (scaleFactor < 1 ? scaleFactor : 1) + ')');
+    }
     this.currentSlide.css({
-      // transform: 'scale(' + (scaleFactor < 1 ? scaleFactor : 1) + ')',
-      transform: 'scale(' + scaleFactor + ')',
       top: Math.floor(($(window).height() - this.currentSlide.outerHeight()) / 2),
       left: Math.floor(($(window).width() - this.currentSlide.outerWidth()) / 2),
     });
   },
-  init: function(selector) {
-    this.slides = $(selector || '.slides').children('.slide');
+  init: function(config) {
+    config = config || {};
+    if (config.scaling) this.config.scaling = config.scaling;
+    this.slides = $(config.selector || '.slides').children('.slide');
     this.slides.each(function(idx) {
       $(this).attr('data-slide-number', idx+1);
     });
-    if (window.location.hash) this.currentSlideNumber = parseInt(window.location.hash.substr(1));
-    this.goToSlide(this.currentSlideNumber);
+    var newSlideNumber = 1;
+    if (window.location.hash) {
+      newSlideNumber = parseInt(window.location.hash.substr(1));
+    }
+    this.goToSlide(newSlideNumber);
     this.normalize();
     setTimeout(this.normalize.bind(this), 100); // Useful if webfonts are loading slowly.
     $(window).resize(this.normalize.bind(this));
@@ -75,7 +111,3 @@ var api = {
     });
   }
 };
-
-$(function() {
-  api.init();
-});
